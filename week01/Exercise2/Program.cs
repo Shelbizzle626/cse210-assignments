2f507557b104bf68b4bc85f0ad733f73d1d30d2a
Shelby Baird <shelbizzle23@hotmail.com>
2026-03-04 21:51:18 -0700

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade average? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter;
+
+        if (grade >= 90)
+    {
+        letter = "A";
     }
+        else if (grade >= 80)
+    {
+        letter = "B";
+    }
+        else if (grade >= 70)
+    {
+        letter = "C";
+    }
+        else if (grade >= 60)
+    {
+        letter = "D";
+    }
+        else
+    {
+        letter = "F";
+    }
+    int lastDigit = grade % 10;
+    string sign;
+
+    if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+    else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+    else
+        {
+            sign = "";
+        }
+    if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+    if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            sign = "";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed the class with a {letter}{sign}!");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}{sign}. Stay focused and you'll get it next time.");
+        }
+    }
+    
 }
