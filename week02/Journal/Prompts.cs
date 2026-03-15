7905d2685023451633563c9441894f057d3c1cbe
Shelby Baird <shelbizzle23@hotmail.com>
2026-03-14 18:17:40 -0600

    WK2 Journal/Abstraction

--- week02/Journal/Prompts.cs
+++ week02/Journal/Prompts.cs
@@ -1,12 +1,21 @@
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 
-public class Prompts
+public class PromptGenerator
 {
-    
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>();
+    public PromptGenerator()
+    {
+        _prompts.Add("What do you feel most accomplished about today? ");
+        _prompts.Add("Did anything make you feel sad today? ");
+        _prompts.Add("Who did you get to visit with today? ");
+        _prompts.Add("Did you feel the spirit today?");
+        _prompts.Add("What did you ask Heavenly Father for help with today? ");
+    }
 
     public string GetRandomPrompt()
 {
-    return "";
+    Random random = new Random();
+    int index = random.Next(_prompts.Count);
+    return _prompts[index];
 }
 }
