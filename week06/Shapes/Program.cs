17c6a84e1f4f49fda662b8cca51cd908382f87c6
Shelby Baird <shelbizzle23@hotmail.com>
2026-04-08 19:45:26 -0600

    WK6 Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("blue", 4, 6));
+        shapes.Add(new Circle("pink", 3));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Area: {s.GetArea()}");
+        }
     }
 }
