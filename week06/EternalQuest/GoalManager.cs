32530c9905361d4341f6ef54114e3ec3322ad086
Shelby Baird <shelbizzle23@hotmail.com>
2026-04-09 21:38:32 -0600

    WK6 game bones2

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,62 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+        _goals = new List<Goal>();
+        _score = 0;
+    }
+
+    public void Start()
+    {
+        bool running = true;
+        while (running)
+        {
+            
+        }
+    }
+
+    public void DisplayPlayerInfo()
+    {
+        
+    }
+
+    public void ListGoalNames()
+    {
+        
+    }
+
+    public void ListGoalDetails()
+    {
+        
+    }
+
+    public void CreateGoal()
+    {
+        
+    }
+
+    public void RecordEvent()
+    {
+        
+    }
+
+    public void SaveGoals()
+    {
+        
+    }
+
+    public void LoadGoals()
+    {
+        
+    }
+
+    
+}
